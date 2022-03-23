6bae29d7efa448a4ef14bac49ba53b840efc6610
Chris Hoffer <chris932100@gmail.com>
2022-03-23 17:08:15 -0400

    Added SVG Wrapper/More SVGs

---
+++ src/Components/svg/SVGArrow/SVGArrow.js
@@ -0,0 +1,9 @@
+
+const SVGArrow = ()=>{
+    return (
+        <svg width="106" height="64" viewBox="0 0 106 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.5" d="M94.6105 0L52.9999 41.6106L11.3893 0L0.194611 11.1947L52.9999 64L105.805 11.1947L94.6105 0Z" fill="currentColor"/>
+        </svg>
+        )
+}
+ export default SVGArrow;
---
+++ src/Components/svg/SVGCircleArrow/SVGCircleArrow.js
@@ -0,0 +1,9 @@
+
+const SVGCircleArrow = ()=>{
+    return(
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.75" d="M24 0C10.752 0 0 10.752 0 24C0 37.248 10.752 48 24 48C37.248 48 48 37.248 48 24C48 10.752 37.248 0 24 0ZM24 28.8L14.4 19.2H33.6L24 28.8Z" fill="currentColor"/>
+    </svg>
+        )
+}
+export default SVGCircleArrow;
