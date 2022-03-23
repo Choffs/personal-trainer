1669ec4b55d45945de9d7af19b1aca8bdd5ee5e2
Chris Hoffer <chris932100@gmail.com>
2022-03-23 16:48:52 -0400

    Responsive Design Section

--- src/Components/UI/Section/Section.js
+++ src/Components/UI/Section/Section.js
@@ -1,14 +1,18 @@
-
+import './Section.scss';
+import SVG from '../../svg/SVG';
 
 const Section =props=>{
     const text = props.text || 'Default Text';
     const title = props.title || 'Default Title';
 
     return (
-        <section className={`${props.className}`}>
-            <div>
+        <section className={`section ${props.className}`}>
+            <div className='section-text-container'>
                 <h2 className='header-text'>{title}</h2>
                 <p className='primary-text'>{text}</p>
+                <span className='section-arrow'>
+                    <SVG>arrow</SVG>
+                </span>
             </div>
         </section>
     )
