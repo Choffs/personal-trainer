460a0e84e1ff73345efb82ff5919f28aa203c9e0
Chris Hoffer <chris932100@gmail.com>
2022-03-23 23:08:43 -0400

    Added arrow button

--- src/Components/UI/ImageCard/ImageCard.js
+++ src/Components/UI/ImageCard/ImageCard.js
@@ -1,11 +1,14 @@
+import SVG from '../../svg/SVG';
 import './ImageCard.scss';
 
 const ImageCard =props=>{
     const src = props.src || '';
     const text = props.text || 'Default';
+    const cardClick = (typeof props.onClick == 'function') ? props.onClick : null;
     return (
-        <div className={`image-card ${props.className}`}>
+        <div onClick={cardClick} className={`image-card ${props.className}`}>
             <h2>{text}</h2>
+            {(cardClick) && <span className='image-card-circle-arrow'><SVG>circle-arrow</SVG></span>}
             <img src={src} alt=''/>
         </div>
     )
