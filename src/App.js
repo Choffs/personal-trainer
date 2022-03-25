96821a52eec870a99bf27740e7f68628b3ff1069
Chris Hoffer <chris932100@gmail.com>
2022-03-25 15:12:19 -0400

    Added Sidebar Functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import Header from './Components/Nav/Header/Header';
+import { useState } from 'react';
 import Landing from './Components/Landing/Landing';
+import Footer from './Components/Nav/Footer/Footer';
+import Sidebar from './Components/UI/SideBar/SideBar';
 import './App.scss';
 
 function App() {
+  const [sidebar, setSidebar] = useState({showSidebar: false, header: '', content: null});
+
+  const openSidebar = (header, content) =>{
+    console.log("open Sidebar")
+    if(!sidebar.showSidebar){
+      setSidebar({showSidebar: true, header: header, content: content});
+    }
+  }
+  const closeSidebar = ()=>{
+      setSidebar({showSidebar: false, header: '', content: null});
+  }
   return (
     <div className="App">
-    <Header></Header>
-    <Landing />
+    <Sidebar closeSidebar={closeSidebar} showSidebar={sidebar.showSidebar} header={sidebar.header}>{sidebar.content}</Sidebar>
+    <Header openSidebar={openSidebar}></Header>
+    <Landing openSidebar={openSidebar}/>
+    <Footer />
     </div>
   );
 }
